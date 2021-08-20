ab4bc395878302a165e99ac2bb87594dd839c701
High_Lander <codywmarkham@gmail.com>
2021-08-19 20:23:18 -0500

    update server typo

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
